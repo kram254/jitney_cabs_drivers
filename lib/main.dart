c9d2f031cca601cce41a56e110d488dd08e9dd68
kram254 <markorlando45@gmail.com>
2021-07-21 17:04:20 +0300

    minorChanges

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,6 @@ import 'package:provider/provider.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-
-
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
   'high_importance_channel', // id
   'High Importance Notifications', // title
